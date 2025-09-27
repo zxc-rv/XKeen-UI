a11658ff1087b4769afef32ac04d10e1c98ebed8
zxc-rv <the.revolution@icloud.com>
2025-09-27 04:33:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+BLUE='\033[1;34m'
+PURPLE='\033[38;2;120;93;200m'
+
 architecture=$(uname -m | tr '[:upper:]' '[:lower:]')
 download_url="https://github.com/zxc-rv/XKeen-UI/releases/latest/download"
 
@@ -28,7 +34,16 @@ case $architecture in
   ;;
 esac
 
+clear
+
 opkg update && opkg install lighttpd lighttpd-mod-fastcgi lighttpd-mod-setenv
+
+if [ -f "/opt/etc/init.d/S80lighttpd" ]; then
+    /opt/etc/init.d/S80lighttpd status
+    if [ $? -eq 0 ]; then
+        /opt/etc/init.d/S80lighttpd stop
+    fi
+fi
 
 cat <<'EOF' >/opt/etc/lighttpd/conf.d/90-xkeenui.conf
 server.port := 1000
@@ -53,19 +68,20 @@ EOF
 
 mkdir -p /opt/share/www/XKeen-UI
 for file in index.html script.js style.css favicon.png; do
-  curl -Lfo /opt/share/www/XKeen-UI/$file https://raw.githubusercontent.com/zxc-rv/XKeen-UI/refs/heads/main/$file
+  curl -Lsfo /opt/share/www/XKeen-UI/$file https://raw.githubusercontent.com/zxc-rv/XKeen-UI/refs/heads/main/$file
 done
 
-curl -Lfo /opt/sbin/xkeen-ui $download_url/$bin && chmod +x /opt/sbin/xkeen-ui
+curl -Lsfo /opt/sbin/xkeen-ui $download_url/$bin && chmod +x /opt/sbin/xkeen-ui
 
 if [ -f "/opt/etc/init.d/S80lighttpd" ] && grep -q "PROCS=lighttpd" /opt/etc/init.d/S80lighttpd; then
-  /opt/etc/init.d/S80lighttpd stop
   sed -i -E "s/^PROCS=lighttpd$/PROCS=\/opt\/sbin\/lighttpd/" /opt/etc/init.d/S80lighttpd
-  /opt/etc/init.d/S80lighttpd start
 fi
+
+/opt/etc/init.d/S80lighttpd start
 
 router_ip=$(ip -f inet addr show dev br0 2>/dev/null | grep inet | sed -n 's/.*inet \([0-9.]\+\).*/\1/p')
 
 echo ""
-echo "Успех!"
-echo "XKeen-UI доступен по адресу: http://$router_ip:1000"
+echo -e "${GREEN}Успех!${NC}"
+echo -e "XKeen-UI доступен по адресу: ${GREEN}http://$router_ip:1000${NC}"
+
