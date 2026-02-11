be5d0615e3ce1b75748a3219bd4e4beb91cec454
zxc-rv <the.revolution@icloud.com>
2026-02-11 04:38:18 +0300

    change repo layout

--- src/version.rs
+++
@@ -1,9 +0,0 @@
-use axum::response::{IntoResponse, Json};
-use crate::types::VERSION;
-
-pub async fn version_handler() -> impl IntoResponse {
-    Json(serde_json::json!({
-        "success": true,
-        "version": VERSION
-    }))
-}
