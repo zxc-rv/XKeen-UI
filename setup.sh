f56c4b238a13d7f157ac096b0b31cbdd9365aadf
zxc-rv <the.revolution@icloud.com>
2026-02-13 01:56:01 +0300

    Update core select label font size
    Change the font size of the core select label to 13px for better
    readability and to match the design.
    
    Update xkeen-ui description

--- setup.sh
+++ setup.sh
@@ -275,7 +275,7 @@ ENABLED=yes
 PROCS=xkeen-ui
 ARGS="-p 1000"
 PREARGS=""
-DESC=$PROCS
+DESC="XKeen UI"
 PATH=/opt/sbin:/opt/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 . /opt/etc/init.d/rc.func
