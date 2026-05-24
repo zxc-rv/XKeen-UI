4e7efc8a264adfb5e80597e6912a7a7cf22a9872
zxc-rv <the.revolution@icloud.com>
2026-05-24 13:23:32 +0300

    Refactor get_arch function and improve command checks
    Refactor get_arch function to use command checks for lscpu and jq installation. Removed redundant hw_id assignment.

--- setup.sh
+++ setup.sh
@@ -41,9 +41,6 @@ spinner() {
 }
 
 get_arch() {
-  local hw_id
-  hw_id=$(curl -sf http://localhost:79/rci/show/version | jq -r '.hw_id | ascii_upcase')
-
   local cpuinfo=$(grep -i 'model name' /proc/cpuinfo | sed -e 's/.*: //i' | tr '[:upper:]' '[:lower:]')
   case "$(uname -m | tr '[:upper:]' '[:lower:]')" in
     *'armv8'* | *'aarch64'* | *'cortex-a'* ) ARCH='arm64-v8a';;
@@ -63,12 +60,14 @@ get_arch() {
   esac
   
   if [[ "$ARCH" = mips32 || "$ARCH" = mips64 ]]; then
-    [ -f /opt/bin/lscpu ] || opkg install lscpu &>/dev/null
+    command -v lscpu &>/dev/null || { opkg update &>/dev/null && opkg install lscpu &>/dev/null; }
     local lscpu_output="$(lscpu 2>/dev/null | tr '[:upper:]' '[:lower:]')"
     echo "$lscpu_output" | grep -q "little endian" && ARCH="${ARCH}le"
   fi
   
   if [[ "$ARCH" == "mips32le" ]]; then
+    command -v jq &>/dev/null || { opkg update &>/dev/null && opkg install jq &>/dev/null; }
+    local hw_id=$(curl -sf http://localhost:79/rci/show/version | jq -r '.hw_id | ascii_upcase')
     case "$hw_id" in
       KN-1710|KN-1711|KN-1713) ARCH="${ARCH}-gnu";;
       *) [ -f /lib/ld-musl-mipsel-sf.so.1 ] || ARCH="${ARCH}-gnu";;
