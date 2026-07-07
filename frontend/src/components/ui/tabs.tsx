edac6e31c986b41e1e37ce93d26c913bbb0e8f9c
zxc-rv <the.revolution@icloud.com>
2026-07-07 17:09:01 +0300

    refactor(frontend): remove radix-ui except tabs component

--- frontend/src/components/ui/tabs.tsx
+++ frontend/src/components/ui/tabs.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { cva, type VariantProps } from 'class-variance-authority'
-import { Tabs as TabsPrimitive } from 'radix-ui'
+import * as TabsPrimitive from '@radix-ui/react-tabs'
 import * as React from 'react'
 
 import { cn } from '@/lib/utils'
